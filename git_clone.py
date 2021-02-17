54c2f9d8817a4228484cb54bc355b0cad8afecf6
Batuhan Gonenc <gonencbthn@pm.me>
2021-02-17 05:04:18 +0300

    fix

--- git_clone.py
+++ git_clone.py
@@ -1,3 +1,10 @@
 import sys, os
-arg = f"git clone https://github.com/{ sys.argv[1] }/{ sys.argv[2] }"
+if len(sys.argv) == 3:
+    arg = f"git clone https://github.com/{ sys.argv[1] }/{ sys.argv[2] } { sys.argv[3]}"
+elif len(sys.argv) == 2:
+    arg = f"git clone https://github.com/{ sys.argv[1] }/{ sys.argv[2] }"
+
+else:
+    arg = "echo '2 or 3 extra required'"
+
 os.system(arg)
